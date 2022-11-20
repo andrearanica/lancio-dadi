5743836eb7880c0494a5660a35dc0550ab424cb4
andrearanica <andrearanica2004@gmail.com>
2022-11-20 09:58:39 +0100

    🚀 Check sum

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 function checkValues (numbers) {
-    let f = true;
+    let f = true
     numbers.map (n => {
         if (n == '' || parseInt(n) < 1) {
-            f = false;
+            f = false
         }
     })
     return f
+}
+
+function checkSum (numbers) {
+    let s = 0
+    numbers.map(n => {
+        s+=parseInt(n)
+    })
+    console.log(s)
+    if (s <= 30) return true
+    else return false
 }
 
 document.getElementById('createDadi').onclick = () => {
     let nums1 = [ document.getElementById('n1_1').value, document.getElementById('n1_2').value, document.getElementById('n1_3').value, document.getElementById('n1_4').value, document.getElementById('n1_5').value, document.getElementById('n1_6').value ]
     let nums2 = [ document.getElementById('n2_1').value, document.getElementById('n2_2').value, document.getElementById('n2_3').value, document.getElementById('n2_4').value, document.getElementById('n2_5').value, document.getElementById('n2_6').value ]
     if (checkValues(nums1) && checkValues(nums2)) {
-        document.getElementById('alert').className = 'alert alert-success'
-        document.getElementById('alert-content').innerHTML = '🚀 Creato'
+        if (checkSum(nums1) && checkSum(nums2)) {
+            document.getElementById('alert').className = 'alert alert-success'
+            document.getElementById('alert-content').innerHTML = '🚀 Creato'
+        } else {
+            document.getElementById('alert').className = 'alert alert-danger'
+            document.getElementById('alert-content').innerHTML = '🚧 La somma dei numeri del dado deve essere minore o uguale a 30'
+        }
     } else {
         document.getElementById('alert').className = 'alert alert-danger'
         document.getElementById('alert-content').innerHTML = '❌ Dati mancanti'
